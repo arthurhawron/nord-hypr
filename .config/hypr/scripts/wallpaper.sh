ff12bf3b67599303c5baa65597e5f952d2d34279
arthurhawron <arthur.j.hawron@gmail.com>
2022-11-29 07:35:59 +1100

    added new files

--- .config/hypr/scripts/wallpaper.sh
+++ .config/hypr/scripts/wallpaper.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-wallpaper=$(find ~/Pictures/Wallpapers/ -type f | shuf -n 1)
-swww init &
+wallpaper=$(find ~/Pictures/Wallpapers/Random -type f | shuf -n 1)
 swww img $wallpaper --transition-type center  --transition-fps 120 &
